e02b9dabc21998a043320de7c7e76427c0e56576
Vrinda Kumar <94823422+vrinda-kumar@users.noreply.github.com>
2025-10-31 20:20:56 +0530

    Add files via upload

---
+++ Q45.c
@@ -0,0 +1,30 @@
+//Q45: Write a program to find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms.
+
+/*
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+Approximate sum: 1.56
+
+Input 2:
+5
+Output 2:
+Approximate sum: 2.22
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int n,i;
+    float s=0;
+    scanf("%d",&n);
+    
+    for(i=1;i<=n;i++)
+        s += (float)(2*i)/(4*i-1);
+        
+    printf("Approximate sum: %.2f",s);
+    
+    return 0;
+}
